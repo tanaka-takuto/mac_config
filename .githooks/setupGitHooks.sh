c12370ab8f4b23048fa93c1eb959a80b85b5fe47
tanaka-takuto <tanaka.takuto.0420@gmail.com>
2024-05-20 09:31:01 +0900

    feat: add setup script for git hooks

---
+++ .githooks/setupGitHooks.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+ThisFilePath=$(realpath "$0")
+TargetPath=$(realpath "$1")
+
+# Get GitHooks Scripts
+FileName=$(basename "$ThisFilePath")
+DirPath=$(dirname "$ThisFilePath")
+GitHookScripts=$(ls $DirPath | grep -v $FileName)
+
+# Check if the target path is provided
+if [ -z "$TargetPath" ]; then
+    echo "Please provide the target path."
+    exit 1
+fi
+if [ ! -d "$TargetPath" ]; then
+    echo "The target path does not exist."
+    exit 1
+fi
+if [ ! -d "$TargetPath/.git/hooks" ]; then
+    echo "The target path is not a git repository."
+    exit 1
+fi
+
+# Copy the git hook scripts to the target path
+for GitHookScript in $GitHookScripts; do
+    cp "$DirPath/$GitHookScript" "$TargetPath/.git/hooks"
+    echo "Copied $GitHookScript to $TargetPath/.git/hooks"
+done
